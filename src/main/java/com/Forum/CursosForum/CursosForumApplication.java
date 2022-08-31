4e95bddd2bab8ca402578285535dbca495a4e3ab
p3rsidh <persidh.pimenta@gmail.com>
2022-08-31 16:02:54 -0300

    Commit inicial

---
+++ src/main/java/com/Forum/CursosForum/CursosForumApplication.java
@@ -0,0 +1,13 @@
+package com.Forum.CursosForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Forum/CursosForum/CursosForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Forum.CursosForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
