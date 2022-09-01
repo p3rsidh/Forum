dbf3bcc281fee61790161b40617d54904cda567e
davispacheco <davipacheco1996@gmail.com>
2022-09-01 12:25:12 -0300

    Adicionar extensão à interface UsuarioRepository.

--- src/main/java/com/Forum/CursosForum/Repository/UsuarioRepository.java
+++ src/main/java/com/Forum/CursosForum/Repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.Forum.CursosForum.Repository;
 
+import com.Forum.CursosForum.Model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository <UsuarioModel, Long> {
 }
