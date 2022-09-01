ebb1da23dc9b4af05049fc7855dade12f64cc1bd
davispacheco <davipacheco1996@gmail.com>
2022-09-01 11:36:16 -0300

    Criar interfaces CursoRepository e UsuarioRepository.

--- src/main/java/com/Forum/CursosForum/Repository/CursoRepository.java
+++ src/main/java/com/Forum/CursosForum/Repository/CursoRepository.java
@@ -3,5 +3,5 @@ package com.Forum.CursosForum.Repository;
 import com.Forum.CursosForum.Model.Usuario.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ForumRepository extends JpaRepository<UsuarioModel, Long> {
+public interface CursoRepository extends JpaRepository<UsuarioModel, Long> {
 }
