f3a01524a1848604f89d98f1187401b3b6340dc5
p3rsidh <persidh.pimenta@gmail.com>
2022-09-01 17:33:14 -0300

    Atualizando Topico controller

--- src/main/java/com/Forum/CursosForum/controller/TopicoController.java
+++ src/main/java/com/Forum/CursosForum/controller/TopicoController.java
@@ -1,12 +1,33 @@
 package com.Forum.CursosForum.controller;
 
+import com.Forum.CursosForum.model.TopicoModel;
 import com.Forum.CursosForum.service.TopicoService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TopicoController {
 
     @Override
-    TopicoService
+    TopicoService topicoService;
+
+    @PostMapping(path = "/forum/topicos")
+    @ResponseStatus
+    public TopicoModel cadastrarTopico(TopicoModel topicoModel){
+        return topicoService.cadastrarTopico(topicoModel);
+    }
+
+    @GetMapping(path = "/forum/topicos")
+    public List<TopicoModel> mostrarTodos(){
+        return topicoService.buscarTopicos();
+    }
+
+    @PutMapping(path = "/forum/topicos/{codigo}")
+    public TopicoModel editar(@PathVariable Long codigo, @RequestBody TopicoModel topicoModel){
+        return topicoService
+    }
+
+
 
 }
