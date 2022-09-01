e514457424f99999567e8ce212934d9fa97c3344
davispacheco <davipacheco1996@gmail.com>
2022-09-01 14:31:31 -0300

    Criar funcionalidade para manipular o usuário do fórum.

--- src/main/java/com/Forum/CursosForum/service/UsuarioService.java
+++ src/main/java/com/Forum/CursosForum/service/UsuarioService.java
@@ -1,7 +1,43 @@
 package com.Forum.CursosForum.service;
 
+import com.Forum.CursosForum.Model.UsuarioModel;
+import com.Forum.CursosForum.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<UsuarioModel> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<UsuarioModel> buscarPorId(Long codigo) {
+        return usuarioRepository.findById(codigo);
+    }
+
+    public UsuarioModel cadastrar(UsuarioModel usuarioModel) {
+        usuarioModel.getCodigo();
+        usuarioModel.getNome();
+        usuarioModel.getEmail();
+        usuarioModel.getSenha();
+        return usuarioRepository.save(usuarioModel);
+    }
+
+    public UsuarioModel alterar(UsuarioModel usuarioModel, Long codigo) {
+        usuarioModel.getCodigo();
+        usuarioModel.getNome();
+        usuarioModel.getEmail();
+        usuarioModel.getSenha();
+        return usuarioRepository.save(usuarioModel);
+    }
+
+    public void deletar(Long codigo) {
+        usuarioRepository.deleteById(codigo);
+    }
 }
