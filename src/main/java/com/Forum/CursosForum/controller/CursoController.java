497a0665eefae7fb4cfda78a9057e1e191468d77
p3rsidh <persidh.pimenta@gmail.com>
2022-09-01 14:44:58 -0300

    Criando metodos do service de Contas

--- src/main/java/com/Forum/CursosForum/controller/CursoController.java
+++ src/main/java/com/Forum/CursosForum/controller/CursoController.java
@@ -1,4 +1,12 @@
-package com.Forum.CursosForum.controller;
+package com.Forum.CursosForum.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.Forum.CursosForum.Service.CursoService;
 
+@RestController
 public class CursoController {
+
+    @Autowired
+   CursoService cursoService;
+
 }
