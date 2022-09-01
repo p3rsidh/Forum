6355be6043fde7de3631b74d376f9b295ffd0325
jhstavares <jhstavares_01@hotmail.com>
2022-09-01 15:25:57 -0300

    Feita a classe RespostaModel

--- src/main/java/com/Forum/CursosForum/Model/RespostaModel.java
+++ src/main/java/com/Forum/CursosForum/Model/RespostaModel.java
@@ -1,4 +1,24 @@
 package com.Forum.CursosForum.Model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "resposta")
 public class RespostaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mensagem;
+
+    @Column(nullable = false)
+    private LocalDate dataCriacao;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+
 }
